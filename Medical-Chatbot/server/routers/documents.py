b261124f656fa494fce970a00c7017afe4202b73
Irfankhan132 <irfanee125@gmail.com>
2026-05-25 11:45:48 +0200

    Delete embeddings from Pinecone

--- Medical-Chatbot/server/routers/documents.py
+++ Medical-Chatbot/server/routers/documents.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter
 from pathlib import Path
+from pinecone import Pinecone
+import os
 
 router = APIRouter()
 
 UPLOAD_DIR = Path("./uploaded_docs")
+
+pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
+index = pc.Index(os.environ["PINECONE_INDEX_NAME"])
 
 @router.get("/documents/")
 async def list_documents():
@@ -19,10 +24,26 @@ async def list_documents():
 
 @router.delete("/documents/{filename}")
 async def delete_document(filename: str):
+
     file_path = UPLOAD_DIR / filename
 
+    # delete local PDF
     if file_path.exists():
         file_path.unlink()
-        return {"message": f"{filename} deleted successfully"}
 
-    return {"error": "File not found"}
+    # delete Pinecone vectors
+    prefix = Path(filename).stem
+
+    vector_ids = []
+
+    response = index.list(prefix=prefix)
+
+    for ids in response:
+        vector_ids.extend(ids)
+
+    if vector_ids:
+        index.delete(ids=vector_ids)
+
+    return {
+        "message": f"{filename} and related vectors deleted successfully"
+    }
